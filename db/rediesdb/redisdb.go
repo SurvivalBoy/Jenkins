ff4cbf6bf4e19172cb13845121c70fa6eda4ad74
王宇航 <wangyuhang@blhcn.com>
2019-08-08 16:17:17 +0800

    添加redisdb实现

--- db/rediesdb/redisdb.go
+++ db/rediesdb/redisdb.go
@@ -1,1 +1,29 @@
-package rediesdb
+package redisdb
+
+import (
+	"github.com/go-redis/redis"
+	"github.com/phjt-go/logger"
+	"github.com/spf13/viper"
+)
+
+// RedisDB Redis的DB对象
+var RedisDB *redis.Client
+
+func Init() {
+	RedisDB = redis.NewClient(&redis.Options{
+		Addr:     viper.GetString("redis_host"),
+		Password: viper.GetString("redis_pass"),
+		DB:       viper.GetInt("redis_db"),
+	})
+
+	defer func() {
+		if r := recover(); r != nil {
+			logger.Error("Redis connection error,", r)
+		}
+	}()
+	_, err := RedisDB.Ping().Result()
+	if err != nil {
+		panic(err)
+	}
+	logger.Info("Redis connection ok")
+}
