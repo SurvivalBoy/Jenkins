9a85fcbc62dbb1893e98d5a66833a14810a7c4c1
王宇航 <wangyuhang@blhcn.com>
2019-08-14 09:41:07 +0800

    撤销run命令

--- build.sh
+++ build.sh
@@ -3,10 +3,7 @@
 docker rmi jenkins:v1.0
 docker build . -t jenkins:v1.0
 
-docker rm -f jenkins
-docker run -itd --restart=unless-stopped -v /etc/localtime:/etc/localtime -v /etc/timezone:/etc/timezone --name jenkins -v $(pwd):/data --network=host jenkins:v1.0
 
-docker logs -f jenkins
 
 
 
